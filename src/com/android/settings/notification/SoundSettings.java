f979bbde795f58c5788f5a1f192398a6018a6127
Ido Ben-Hur <idoybh2@gmail.com>
2021-03-14 17:33:49 +0200

    Settings: Allow choosing volume panel position [2/2]
    Default to the DT set overlay config_audioPanelOnLeftSide
    
    Signed-off-by: dlwlrma123 <alexfinhart@gmail.com>
    Signed-off-by: afterallafk <shivamatiet2001@gmail.com>

--- src/com/android/settings/notification/SoundSettings.java
+++ src/com/android/settings/notification/SoundSettings.java
@@ -37,6 +37,7 @@ import com.android.settings.core.OnActivityResultListener;
 import com.android.settings.dashboard.DashboardFragment;
 import com.android.settings.search.BaseSearchIndexProvider;
 import com.android.settings.sound.HandsFreeProfileOutputPreferenceController;
+import com.android.settings.sound.VolumeDialogPositionPreferenceController;
 import com.android.settings.widget.PreferenceCategoryController;
 import com.android.settingslib.core.AbstractPreferenceController;
 import com.android.settingslib.core.instrumentation.Instrumentable;
@@ -267,7 +268,10 @@ public class SoundSettings extends DashboardFragment implements OnActivityResult
                 new EmergencyTonePreferenceController(context, fragment, lifecycle);
         final ScreenshotSoundPreferenceController screenshotSoundPreferenceController =
                 new ScreenshotSoundPreferenceController(context, fragment, lifecycle);
+        final VolumeDialogPositionPreferenceController volumeDialogPositionPreferenceController =
+                new VolumeDialogPositionPreferenceController(context);
 
+        controllers.add(volumeDialogPositionPreferenceController);
         controllers.add(dialPadTonePreferenceController);
         controllers.add(screenLockSoundPreferenceController);
         controllers.add(chargingSoundPreferenceController);
